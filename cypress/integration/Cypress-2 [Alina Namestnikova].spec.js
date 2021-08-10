3eeb6297c15aaa3453f7f681a77fcd728eb9a6a5
iamalina <alina.namestnikova@gmail.com>
2021-08-10 23:08:27 +0300

    save

--- cypress/integration/Cypress-2 [Alina Namestnikova].spec.js
+++ cypress/integration/Cypress-2 [Alina Namestnikova].spec.js
@@ -1,43 +1,31 @@
 /// <reference types="cypress" />
 
-describe('Successful Login', () => {
+describe('Login tests', () => {
     beforeEach(() => {
       cy.visit('https://the-internet.herokuapp.com/login');
     });
 
-    it('displays two todo items by default', () => {
+    it('Successful Login', () => {
        cy.get('#username').type('tomsmith');
        cy.get('#password').type('SuperSecretPassword!');
        cy.get('.radius').click();
        cy.get('#flash').should('contain', 'You logged into a secure area!')
       });
-    });
 
-describe('Failed Login', () => {
-    beforeEach(() => {
-      cy.visit('https://the-internet.herokuapp.com/login');
-    });
-
-    it('displays two todo items by default', () => {
-       cy.get('#username').type('кобра');
-       cy.get('#password').type('123');
-       cy.get('.radius').click();
-       cy.get('#flash').should('contain', 'Your username is invalid!')
-    });
-});
-
-describe('Logout', () => {
-    beforeEach(() => {
-        cy.visit('https://the-internet.herokuapp.com/login');
-    });
-    
-    it('displays two todo items by default', () => {
-        cy.get('#username').type('tomsmith');
-        cy.get('#password').type('SuperSecretPassword!');
+      it('Failed login', () => {
+        cy.get('#username').type('кобра');
+        cy.get('#password').type('123');
         cy.get('.radius').click();
-        cy.get('#flash').should('contain', 'You logged into a secure area!');
+        cy.get('#flash').should('contain', 'Your username is invalid!')
+     });
 
-        cy.get('[href="/logout"]').click(); 
-        cy.get('#flash.success').should('contain', 'You logged out of the secure area!');
-    });
+     it('Logout', () => {
+      cy.get('#username').type('tomsmith');
+      cy.get('#password').type('SuperSecretPassword!');
+      cy.get('.radius').click();
+      cy.get('#flash').should('contain', 'You logged into a secure area!');
+
+      cy.get('[href="/logout"]').click(); 
+      cy.get('#flash.success').should('contain', 'You logged out of the secure area!');
+  });
 });
